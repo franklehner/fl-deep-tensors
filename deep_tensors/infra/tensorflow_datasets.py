09fa4945f84c1f62291c88dfe482f1e1382a15dc
Frank Lehner <frank-lehner71@t-online.de>
2024-02-08 12:20:20 +0100

    infra/tensorflow_datasets: Add validation

--- deep_tensors/infra/tensorflow_datasets.py
+++ deep_tensors/infra/tensorflow_datasets.py
@@ -1,14 +1,18 @@
 "Get Tensorflow datasets"
-from typing import Tuple
+from typing import Literal, Tuple
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+from deep_tensors.models.schemas import TensorflowDataSet
+
 
 TrainTest = Tuple[tf.data.Dataset, tf.data.Dataset]
+Name = Literal["mnist"]
 
 
-def get_dataset(name: str) -> TrainTest:
+def get_dataset(name: Name) -> TrainTest:
     "Get tensorflow dataset"
+    TensorflowDataSet(name=name)
     builder: tfds.core.DatasetBuilder = tfds.builder(name)
     builder.download_and_prepare()
     datasets: tf.data.Dataset = builder.as_dataset(shuffle_files=False)
--- test/infra/test_tensorflow_datasets.py
+++ test/infra/test_tensorflow_datasets.py
@@ -18,3 +18,9 @@ def test_get_dataset(dataset, shape, length):
     assert tuple(train_orig.element_spec["image"].shape) == shape
     assert tuple(test_orig.element_spec["image"].shape) == shape
     assert len(train_orig) == length
+
+
+def test_get_dataset_with_invalid_name():
+    "Test case for invalid dataset"
+    with pytest.raises(ValueError):
+        td.get_dataset(name="foo")
