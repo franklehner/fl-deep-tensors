8393d5cab1b746270046bd4c39e4fe79acbe46ab
Frank Lehner <frank-lehner71@t-online.de>
2024-02-08 12:21:38 +0100

    models/schemas: Add validation for tensorflow_datasets

--- deep_tensors/models/schemas.py
+++ deep_tensors/models/schemas.py
@@ -2,7 +2,7 @@
 """
 from typing import List, Literal, Tuple
 
-from pydantic import BaseModel, PositiveInt, validator
+from pydantic import BaseModel, PositiveInt, validator, field_validator
 
 
 class DenseChecker(BaseModel):
@@ -33,3 +33,33 @@ class BaseModelCompiler(BaseModel):
     optimizer: Literal["adam", "rmsprop", "sgd"]
     loss: Literal["binary_crossentropy", "categorical_crossentropy", "mae", "mse"]
     metrics: List[Literal["accuracy", "binary_accuracy", "f1", "r2"]]
+
+
+class MnistPrepareData(BaseModel):
+    "Check MnistData class"
+    buffer_size: PositiveInt
+    batch_size: PositiveInt
+    valid_size: PositiveInt
+
+    @field_validator("valid_size")
+    @classmethod
+    def check_valid_size(cls, value):
+        "check the length of the valid field"
+        if value >= 60000:
+            raise ValueError("The size of the valid_data must not be bigger than train size")
+
+        return value
+
+    @field_validator("batch_size")
+    @classmethod
+    def check_batch_size(cls, value):
+        "Check the length of the batch_size"
+        if value >= 60000:
+            raise ValueError("The batch_size must not be bigger than train_size")
+
+        return value
+
+
+class TensorflowDataSet(BaseModel):
+    "Check Tensorflow DataSet"
+    name: Literal["mnist"]
